9154944e23027a213746f904e9828d433736fdc9
Semen Tonkikh <tonksemen@gmail.com>
2025-07-27 18:28:16 +0300

    remove hover border, and fix crush

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,25 +1,13 @@
 return {
   "folke/noice.nvim",
   opts = {
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true,
-    },
-    notify = {
+    norify = {
       enabled = false,
     },
     lsp = {
       enabled = true,
       hover = {
-        enabled = true,
-        silent = true,
-        opts = {
-          border = "rounded",
-          position = {
-            row = 2,
-          },
-        },
+        enabled = false,
       },
       signature = {
         enabled = true,
