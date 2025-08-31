4e975d457745ef5e9bda3440be9d41c9daac543d
Semen Tonkikh <tonksemen@gmail.com>
2025-08-31 12:35:49 +0300

    fix(catppuccin): fix bufferline bug

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,21 +2,30 @@ return {
   "catppuccin/nvim",
   name = "catppuccin",
   lazy = false,
-  priority = 1000,
-  opts = {
-    flavour = "frappe",
-    transparent_background = true,
-    float = {
+  opts = function(_, opts)
+    -- твои базовые настройки
+    opts.flavour = "frappe"
+    opts.transparent_background = true
+    opts.float = {
       transparent = true,
       solid = true,
-    },
-    integrations = {
+    }
+    opts.integrations = {
       telescope = true,
       cmp = true,
       lsp_trouble = true,
       mason = true,
-    },
-  },
+    }
+
+    -- фикс бага с bufferline
+    local module = require("catppuccin.groups.integrations.bufferline")
+    if module then
+      module.get = module.get_theme
+    end
+
+    return opts
+  end,
+  priority = 1000,
   config = function(_, opts)
     require("catppuccin").setup(opts)
     vim.cmd.colorscheme("catppuccin")
